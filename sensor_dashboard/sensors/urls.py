afc84d0966f99a1f29beb60edada6e815a6f23b2
preranadhake <dhakeprerana04@gmail.com>
2024-11-09 11:51:42 +0530

    crop disease predicted

--- sensor_dashboard/sensors/urls.py
+++ sensor_dashboard/sensors/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('dashboard/', views.dashboard, name='dashboard'),
+    path('', views.dashboard, name='dashboard'),
     path('api/data/', views.get_latest_sensor_data, name='get_latest_sensor_data'),
     path('toggle_pump/', views.toggle_pump, name='toggle_pump'),
     path('pump_control/', views.pump_control, name='pump_control'),  # Add this line for pump control page
