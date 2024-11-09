bd7f6bb0c2a70c813c88178047927f9094c2b709
preranadhake <dhakeprerana04@gmail.com>
2024-11-09 11:37:07 +0530

    Navigation bar and pump on/off button is added

--- sensor_dashboard/sensors/urls.py
+++ sensor_dashboard/sensors/urls.py
@@ -2,6 +2,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.dashboard, name='dashboard'),
+    path('dashboard/', views.dashboard, name='dashboard'),
     path('api/data/', views.get_latest_sensor_data, name='get_latest_sensor_data'),
+    path('toggle_pump/', views.toggle_pump, name='toggle_pump'),
+    path('pump_control/', views.pump_control, name='pump_control'),  # Add this line for pump control page
+
 ]
